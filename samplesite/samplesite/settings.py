b42e9141cbce264ccb0ee79dc6eef804292fcb71
Maxim <mogilevskiymv@gmail.com>
2024-10-28 20:49:27 +0300

    Переложил базу данных в каталог проекта

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'samplesite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': Path('/mnt/d') / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': Path('/mnt/d') / 'db.sqlite3',
     }
 }
 
